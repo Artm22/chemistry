95dc4779d1838144702e6c2957ffa58dd548afba
Artm22 <lbxlma@gmail.com>
2022-12-03 18:46:39 +0300

    First Commit

---
+++ Химия.py
@@ -0,0 +1,8 @@
+import math
+
+a = int(input(print("Введите индекс 1 : ")))
+b = int(input(print("Введите индекс 2 : ")))
+c = int(input(print("Введите коэфициент : ")))
+
+y = (b * c)/a
+print( y * -1)
